0bab72f9d154d1ebf2ad25a1bbb7e156c51b3cd3
Austin Papritz <austin@papritz.dev>
2023-03-01 16:43:31 -0800

    npm tone

---
+++ src/app/synth.js
@@ -0,0 +1,9 @@
+import * as Tone from "tone";
+
+export function sine() {
+  let osc = new Tone.Oscillator();
+  osc.type = "sine"; // triangle, square or sawtooth
+  osc.frequency.value = 80; // hz
+  osc.start();
+  osc.toDestination();
+}
